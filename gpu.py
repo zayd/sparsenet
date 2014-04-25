de7757ddd8e8edf306198946b6d377d50efbcc53
S. Zayd Enam <zayd@berkeley.com>
2014-04-24 18:11:51 -0700

    GPU bigfixes

--- gpu.py
+++ gpu.py
@@ -8,13 +8,13 @@ import math
 import scipy.sparse.linalg
 import scipy.sparse as sps
 
-def fista(I, Phi, lambdav, tol=10e-6, max_iterations=25, display=True, L=None):
-	""" GPU FISTA 
-	I: Images
-	Phi: Dictionary
-	lambdav: Sparse Penalty
-	L = Largest eigenvalue of Phi
+def fista(I, Phi, lambdav, L=None, tol=10e-6, max_iterations=200, display=True, verbose=False):
 	"""
+	I: Images 
+ 	Phi: Dictionary 
+ 	lambdav: Sparse Penalty 
+ 	L = Largest eigenvalue of Phi 
+ 	"""
 
 	b = numbapro.cudalib.cublas.Blas()
 	(m, n) = Phi.shape
@@ -22,6 +22,7 @@ def fista(I, Phi, lambdav, tol=10e-6, max_iterations=25, display=True, L=None):
 
 	if L == None:
 		L = scipy.sparse.linalg.svds(Phi, 1, which='LM', return_singular_vectors=False)
+		print "Max eigenvalue: ." + str(L)
 
 	L = (L**2)*2 # L = svd(Phi) -> eig(2*(Phi.T*Phi))
 	invL = 1/L
@@ -32,19 +33,30 @@ def fista(I, Phi, lambdav, tol=10e-6, max_iterations=25, display=True, L=None):
 
 	d_I = cuda.to_device(np.array(I, dtype=np.float32, order='F'))
 	d_Phi = cuda.to_device(np.array(Phi, dtype=np.float32, order='F'))
-	d_Q = cuda.device_array((n, n), dtype=np.float32)
-	d_c = cuda.device_array((n, batch), dtype=np.float32)
+	d_Q = cuda.device_array((n, n), dtype=np.float32, order='F')
+	d_c = cuda.device_array((n, batch), dtype=np.float32, order='F')
 	d_x = cuda.to_device(np.array(np.zeros((n, batch), dtype=np.float32), order='F'))
 	d_y = cuda.to_device(np.array(np.zeros((n, batch), dtype=np.float32), order='F'))
 	d_x2 = cuda.to_device(np.array(np.zeros((n, batch), dtype=np.float32), order='F'))
+
+	# Temporary array variables
+	d_t = cuda.device_array((m, batch), dtype=np.float32, order='F')
+	d_t2 = cuda.device_array(n*batch, dtype=np.float32, order='F')
 
 	b.gemm('T', 'N', n, n, m, 1, d_Phi, d_Phi, 0, d_Q) 	# Q = Phi^T * Phi
 	b.gemm('T', 'N', n, batch, m, -2, d_Phi, d_I, 0, d_c) # c = -2*Phi^T * y
 
 	blockdim = 32, 32
-	griddim = int(math.ceil(m/blockdim[0])), int(math.ceil(batch/blockdim[1]))
+	griddim = int(math.ceil(n/blockdim[0])), int(math.ceil(batch/blockdim[1]))
 
-	for i in range(max_iterations):
+	blockdim_1d = 256
+	griddim_1d = int(math.ceil(n*batch/blockdim_1d))
+
+	start = l2l1obj(b, d_I, d_Phi, d_x, d_t, d_t2, lambdav, blockdim_1d, griddim_1d)
+	obj2 = start
+
+	for i in xrange(max_iterations):
+
 		# x2 = 2*Q*y + c
 		b.symm('L', 'U', n, batch, 2, d_Q, d_y, 0, d_x2)
 		b.geam('N', 'N', n, batch, 1, d_c, 1, d_x2, d_x2)
@@ -63,29 +75,36 @@ def fista(I, Phi, lambdav, tol=10e-6, max_iterations=25, display=True, L=None):
 		b.geam('N', 'N', n, batch, 1, d_x2, 0, d_x, d_x)
 		t = t2
 
+		# update objective
+		obj = obj2
+		obj2 = l2l1obj(b, d_I, d_Phi, d_x2, d_t, d_t2, lambdav, blockdim_1d, griddim_1d)
+
+		if verbose:
+			x2 = d_x2.copy_to_host()
+			print "L1 Objective: " + str(obj2)
+
+		if np.abs(obj-obj2)/float(obj) < tol:
+			break
+
 	x2 = d_x2.copy_to_host()
 
-	if display == True:
+	if display:
+		print "FISTA Iterations: " + str(i)
 		print "L1 Objective: " +  str(lambdav*np.sum(np.abs(x2)) + np.sum((I-Phi.dot(x2))**2))
-		#print "L1 Objective: " + str(l2l1obj(b, d_Q, d_c, d_I, d_x2, lambdav, n, griddim, blockdim))
+		print "Objective delta: " + str(obj2-start)
 
 	return x2
 
-def l2l1obj(b, d_I, d_Phi, d_x2, lambdav):
+def l2l1obj(b, d_I, d_Phi, d_x2, d_t, d_t2, lambdav, blockdim, griddim):
 	(m, n) = d_Phi.shape
 	(m, batch) = d_I.shape
-	blockdim = 256
-	griddim = int(math.ceil(n*batch/float(blockdim)))
-
-	d_t = cuda.to_device(np.array(np.zeros((m, batch), dtype=np.float32), order='F'))
 
 	b.gemm('N', 'N', m, batch, n, 1, d_Phi, d_x2, 0, d_t)
 	b.geam('N', 'N', m, batch, 1, d_I, -1, d_t, d_t)
 
- 	l2 = b.nrm2(d_t.reshape(m*batch))**2
+ 	l2 = b.nrm2(d_t.ravel(order='F'))**2
  	
- 	d_t2 = d_x2.reshape(n*batch)
- 	gabs[griddim, blockdim](d_t2)
+ 	gabs[griddim, blockdim](d_x2.ravel(order='F'), d_t2)
  	
  	l1 = lambdav*b.asum(d_t2)
 
@@ -99,26 +118,28 @@ def l1prox(A, t, C):
 	C: output (dim, batch) """
 	i, j = cuda.grid(2)
 
-	if j >= A.shape[0] or i >= A.shape[1]:
+	if i >= A.shape[0] or j >= A.shape[1]:
 		return
 
-	if A[j, i] >= t:
-		C[j, i] = A[j, i] - t 
-	elif A[j, i] <= -t: 
-		C[j, i] = A[j, i] + t    
+	if A[i, j] >= t:
+		C[i, j] = A[i, j] - t 
+	elif A[i, j] <= -t: 
+		C[i, j] = A[i, j] + t    
 	else:
-		C[j, i] = 0
+		C[i, j] = 0
 
 	return
 
-@cuda.jit('void(float32[:])')
-def gabs(x):
+@cuda.jit('void(float32[:], float32[:])')
+def gabs(x, y):
 	i = cuda.grid(1)
 
-	if i > x.size:
+	if i >= x.size:
 		return
 
 	if x[i] < 0:
-		x[i] = -x[i]
+		y[i] = -x[i]
+	else:
+		y[i] = x[i]
 
 	return
